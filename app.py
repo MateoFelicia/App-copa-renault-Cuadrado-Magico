314830ecdca54b7fad43b69e338e67738231735a
Mateofelicia <d48011357@alumnos.itr.edu.ar>
2026-05-14 11:34:45 -0300

    Actualizando css

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
+
+from flask import redirect, url_for
 
 app = Flask(__name__)
 
-# Página principal
+# # Ruta principal
+# @app.route("/")
+# def inicio():
+#     return render_template("index.html")
+
 @app.route("/")
-def inicio():
+def inicio_():
     return render_template("inicio.html")
 
 @app.route("/partidos", defaults={"categoria": "Todas", "deporte": "Todos", "genero": "Todos"})
@@ -12,10 +19,40 @@ def inicio():
 def partidos(categoria, deporte, genero):
 
     todos = [
-        {"hora": "16:30", "equipo_local": "Renault B", "equipo_visitante": "San Martín", "categoria": "Menor", "genero": "Masculino", "deporte": "Fútbol", "estado": "prox"},
-        {"hora": "18:00", "equipo_local": "Renault A", "equipo_visitante": "Godoy Cruz", "categoria": "Mayor", "genero": "Femenino", "deporte": "Fútbol", "estado": "prox"},
-        {"hora": "19:00", "equipo_local": "Renault C", "equipo_visitante": "Huracán", "categoria": "Menor", "genero": "Mixto", "deporte": "Básquet", "estado": "prox"},
-    ]
+    {
+        "hora": "16:30",
+        "equipo_local": "Renault B",
+        "equipo_visitante": "San Martín",
+        "categoria": "Menor",
+        "genero": "Masculino",
+        "deporte": "Fútbol",
+        "estado": "live",
+        "goles_local": 2,
+        "goles_visitante": 1
+    },
+
+    {
+        "hora": "18:00",
+        "equipo_local": "Renault A",
+        "equipo_visitante": "Godoy Cruz",
+        "categoria": "Mayor",
+        "genero": "Femenino",
+        "deporte": "Fútbol",
+        "estado": "prox"
+    },
+
+    {
+        "hora": "19:00",
+        "equipo_local": "Renault C",
+        "equipo_visitante": "Huracán",
+        "categoria": "Menor",
+        "genero": "Mixto",
+        "deporte": "Básquet",
+        "estado": "terminado",
+        "goles_local": 88,
+        "goles_visitante": 74
+    },
+]
 
     filtrados = []
 
@@ -48,6 +85,41 @@ def noticias():
 @app.route("/mas")
 def mas():
     return render_template("mas.html")
+
+# @app.route("/registro", methods=["POST"])
+# def registro():
+#     nombre = request.form["nombre"]
+#     email = request.form["email"]
+#     password = request.form["password"]
+
+#     conexion = sqlite3.connect("database.db")
+#     cursor = conexion.cursor()
+
+#     cursor.execute(
+#         "INSERT INTO usuarios (nombre, email, password) VALUES (?, ?, ?)",
+#         (nombre, email, password)
+#     )
+
+#     conexion.commit()
+#     conexion.close()
+
+#     from flask import redirect, url_for
+
+#     return redirect(url_for("usuarios"))
+
+# @app.route("/usuarios")
+# def usuarios():
+#     import sqlite3
+
+#     conexion = sqlite3.connect("database.db")
+#     cursor = conexion.cursor()
+
+#     cursor.execute("SELECT * FROM usuarios")
+#     datos = cursor.fetchall()
+
+#     conexion.close()
+
+#     return render_template("usuarios.html", usuarios=datos)
 
 
 if __name__ == "__main__":
