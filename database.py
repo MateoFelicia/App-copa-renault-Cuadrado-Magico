fe6a7dfcf461b2ef3b36e9f6c80f86e0e08b48e1
PabloCALA18 <d49499480@alumnos.itr.edu.ar>
2026-05-26 07:57:17 -0300

    fireBase

--- database.py
+++ database.py
@@ -1,20 +1,14 @@
-import sqlite3
-from flask import g, current_app
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-def get_db():
-    # g es un objeto especial de Flask que dura exactamente un request
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            "database.db",
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row  # permite acceder columnas por nombre
-    return g.db
-
-def close_db(e=None):
-    db = g.pop('db', None)
-    if db is not None:
-        db.close()
+_db = None
 
 def init_app(app):
-    app.teardown_appcontext(close_db)  # cierra la conexión al final de cada request
+    global _db
+    cred = credentials.Certificate("firebase_credentials.json")
+    firebase_admin.initialize_app(cred)
+    _db = firestore.client()
+    app.extensions['firestore'] = _db
+
+def get_db():
+    return _db
