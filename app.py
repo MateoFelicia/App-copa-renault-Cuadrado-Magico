52118342838d44fb1645eb2737e622c6c8431276
MisaelZanetti <misaelzanetti@gmail.com>
2026-05-28 08:11:54 -0300

    Incorporacion de pantalla admin con sus formularios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from database import database
+import database
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,7 +12,9 @@ from routes.sponsors import sponsors_bp
 from routes.usuarios import usuarios_bp
 from routes.registros import registro_bp
 from routes.partidos import partidos_bp
+from routes.admin import admin_bp
 
+app.register_blueprint(admin_bp)
 app.register_blueprint(inicio_bp)
 app.register_blueprint(noticias_bp)
 app.register_blueprint(cantina_bp)
