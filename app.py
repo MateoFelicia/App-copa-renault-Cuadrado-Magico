951f1c87a68f56aa5ecbc3b8530a23a1978b0eb7
MisaelZanetti <misaelzanetti@gmail.com>
2026-06-11 10:01:51 -0300

    Pantalla de login admin

--- app.py
+++ app.py
@@ -13,8 +13,10 @@ from routes.usuarios import usuarios_bp
 from routes.registros import registro_bp
 from routes.partidos import partidos_bp
 from routes.admin import admin_bp
+from routes.auth import auth_bp
 
 app.register_blueprint(admin_bp)
+app.register_blueprint(auth_bp)
 app.register_blueprint(inicio_bp)
 app.register_blueprint(noticias_bp)
 app.register_blueprint(cantina_bp)
