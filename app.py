75b6814fcc49b52fc2ecbf09b52e7dcc86cf6fcf
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2026-06-12 07:47:35 -0300

    autenticación (casi termino, faltan detalles)

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import database
 from flask import Flask
-import firebase_admin
-from firebase_admin import credentials, auth
 
 app = Flask(__name__)
 app.secret_key = "cuadradomagicoamaDios"
 database.init_app(app)
-
-cred = credentials.Certificate("firebase_credentials.json")
-firebase_admin.initialize_app(cred)
 
 from routes.inicio import inicio_bp
 from routes.noticias import noticias_bp
@@ -19,11 +14,7 @@ from routes.usuarios import usuarios_bp
 from routes.registros import registro_bp
 from routes.partidos import partidos_bp
 from routes.admin import admin_bp
-<<<<<<< HEAD
-from routes.admin_login import login_bp
-=======
 from routes.auth import auth_bp
->>>>>>> 951f1c87a68f56aa5ecbc3b8530a23a1978b0eb7
 
 app.register_blueprint(admin_bp)
 app.register_blueprint(auth_bp)
--- routes/admin_login.py
+++ routes/admin_login.py
@@ -1,14 +1,36 @@
-from flask import Blueprint, request, redirect, url_for, jsonify, session, redirect
-from database import get_db
+from flask import Blueprint, request, redirect, url_for, jsonify, session, redirect, render_template
 import firebase_admin
 from firebase_admin import credentials, auth
 
-admin_bp = Blueprint('admin', __name__)
+login_bp = Blueprint('admin_login', __name__)
 
-cred = credentials.Certificate("ruta/al/archivo.json")
+cred = credentials.Certificate("firebase_credentials.json")
 firebase_admin.initialize_app(cred)
 
 auth.set_custom_user_claims("Iw29cbeyWASl4VUM5BTgzCOl8Qr1", {"rol": "superadmin"})
 auth.set_custom_user_claims("zvCLe3q0DySAZtGNYQ5rMNbGlpZ2", {"rol": "superadmin"})
 auth.set_custom_user_claims("2NdttyMAqJNNr5SfJaQLhcgNCa83", {"rol": "superadmin"})
 auth.set_custom_user_claims("BNw7TUA6xVXhTtD6F6vEYQA4UNp2", {"rol": "superadmin"})
+
+@login_bp.route('/admin/verify', methods=['POST'])
+def verify_token():
+    id_token = request.json.get('idToken')
+    
+    decoded_token = auth.verify_id_token(id_token)
+    rol = decoded_token.get('rol')
+    if not rol:
+            return jsonify({'error': 'No autorizado'}), 403
+    session['rol'] = rol
+    session['uid'] = decoded_token['uid']
+    return jsonify({'ok': True, 'rol': rol})
+
+@login_bp.route('/admin/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login.admin_login_page'))
+
+@login_bp.route('/admin-login')
+def admin_login_page():
+    if session.get('rol'):
+        return redirect('templates/admin/dashboard')
+    return render_template('templates/admin/login.html')
