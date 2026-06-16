294478c6b87210a2c38958146ac8e5969c75890f
LoloNalbi23 <d48599689@alumnos.itr.edu.ar>
2026-06-16 08:39:13 -0300

    Eliminando archivos innecesarios

--- app.py
+++ app.py
@@ -10,23 +10,17 @@ from routes.noticias import noticias_bp
 from routes.cantina import cantina_bp
 from routes.croquis import croquis_bp
 from routes.sponsors import sponsors_bp
-from routes.usuarios import usuarios_bp
-from routes.registros import registro_bp
 from routes.partidos import partidos_bp
 from routes.admin import admin_bp
-# from routes.auth import auth_bp
 from routes.admin_login import login_bp
 
 app.register_blueprint(admin_bp)
 app.register_blueprint(login_bp)
-# app.register_blueprint(auth_bp)
 app.register_blueprint(inicio_bp)
 app.register_blueprint(noticias_bp)
 app.register_blueprint(cantina_bp)
 app.register_blueprint(croquis_bp)
 app.register_blueprint(sponsors_bp)
-app.register_blueprint(usuarios_bp)
-app.register_blueprint(registro_bp)
 app.register_blueprint(partidos_bp)
 
 if __name__ == "__main__":
--- routes/auth.py
+++
@@ -1,7 +0,0 @@
-# from flask import Blueprint, render_template
-
-# auth_bp = Blueprint('auth', __name__)
-
-# @auth_bp.route("/admin-login")
-# def login():
-#     return render_template("admin/login.html", error=None)
